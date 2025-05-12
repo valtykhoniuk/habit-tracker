eb58448a689a6c8358fd1d62cab73789c78f3708
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-12 18:14:31 +0200

    bg image as import added

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./output.css";
 import App from "./App.tsx";
+import bgImage from "./assets/image.png";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <App />
+    </div>
   </StrictMode>
 );
